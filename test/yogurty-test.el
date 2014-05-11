0b264374215fc1fb30b8e92dbdb6774de18cfbc4
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2014-05-11 09:25:11 -0700

    Add second test.

--- test/yogurty-test.el
+++ test/yogurty-test.el
@@ -1,5 +1,9 @@
 (defvar rt-test-string "[rt.example.com #2341] opt-out for google analytics on website")
 
-(ert-deftest yogurty/find-ticket-from-string ()
+(ert-deftest yogurty/find-ticket-number-from-string ()
   "Should find ticket subject from string."
-  (should (equal (yogurty-find-rt-ticket-subject-from-string rt-test-string) "2341")))
+  (should (equal (yogurty-find-rt-ticket-number-from-string rt-test-string) "2341")))
+
+(ert-deftest yogurty/find-ticket-subject-from-string ()
+  "Should find ticket subject from string."
+  (should (equal (yogurty-find-rt-ticket-subject-from-string rt-test-string) "opt-out for google analytics on website")))
