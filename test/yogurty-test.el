65cf166f5801d7a4acf31a3c4a19a890f1945664
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2014-05-12 00:04:01 -0700

    Learn about fixtures; taken from Emacs ERT manual.

--- test/yogurty-test.el
+++ test/yogurty-test.el
@@ -1,5 +1,28 @@
 (defvar rt-test-string "[rt.example.com #2341] opt-out for google analytics on website")
 
+;; (defun my-fixture (body)
+;;   (unwind-protect
+;;       (progn [set up]
+;; 	     (funcall body))
+;;     [tear down]))
+
+;;      (ert-deftest my-test ()
+;;        (my-fixture
+;;         (lambda ()
+;;           [test code])))
+
+(defun my-fixture (body)
+  (unwind-protect
+      (progn
+	(with-temp-buffer
+	  (insert-string "From: <aardvark@saintaardvarkthecarpeted.com>")
+	  (insert-string "To: <rt@example.com>")
+	  (insert-string "Subject: [rt.example.com #2341] opt-out for google analytics on website")
+	  (goto-char (point-min))
+	  (funcall body)))))
+
+; (my-fixture (lambda () (message (yogurty-find-rt-ticket-subject-from-rt-email))))
+
 (ert-deftest yogurty/find-ticket-number-from-string ()
   "Should find ticket subject from string."
   (should (equal (yogurty-find-rt-ticket-number-from-string rt-test-string) "2341")))
