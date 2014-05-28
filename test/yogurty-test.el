6ef5c619055a40a5b6146726b39a8fe3a0afa91b
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2014-05-28 08:41:43 -0700

    Another test -- but this should not work.

--- test/yogurty-test.el
+++ test/yogurty-test.el
@@ -91,3 +91,11 @@
   "Should find already-existing RT ticket in org buffer."
   (my-org-file-fixture
    (lambda () (should (equal (yogurty-find-rt-ticket-in-org-file "2343") 3)))))
+
+;; FIXME: This test is passing, and it shouldn't be!
+(ert-deftest yogurty/insert-rt-ticket-in-org-file ()
+  "Should find already-existing RT ticket in org buffer."
+  (my-org-file-fixture
+   (progn
+     (yogurty-insert-rt-ticket-into-org-generic "2346" "eBiz it up a notch")
+     (lambda () (should (equal (yogurty-find-rt-ticket-in-org-file "2346") "4"))))))
