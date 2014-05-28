ba6bc15d4f3211392aac87c744b7165c7e1e1f0c
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2014-05-27 20:05:37 -0700

    Silly typo was keeping this test from working.

--- test/test-helper.el
+++ test/test-helper.el
@@ -23,9 +23,9 @@
 (defmacro with-sandbox-org-file (&rest body)
   "Evaluate BODY using sandbox org file."
   `(let ((default-directory root-sandbox-path)
-	 (yogurty-org-file (f-join root-sandbox "test.org"))
+	 (yogurty-org-file (f-join root-sandbox-path "test.org")))
      (when (f-dir? root-sandbox-path)
        (f-delete root-sandbox-path :force))
      (f-mkdir root-sandbox-path)
      (f-touch (f-join root-sandbox-path "test.org"))
-     ,@body)))
+     ,@body))
