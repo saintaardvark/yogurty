960c449f9ee90806b22d7c57e586bdc3996372cf
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2014-05-28 17:50:06 -0700

    Different test text to distinguish probs in different fixtures.

--- test/yogurty-test.el
+++ test/yogurty-test.el
@@ -36,9 +36,9 @@
       (progn
 	(with-sandbox-org-file
 	 (set-buffer (find-file-noselect yogurty-org-file))
-	 (insert-string "* RT #2341 -- Big cloud my .emacs file\n")
-	 (insert-string "** RT #2342 -- opt-out for google analytics on website\n")
-	 (insert-string "*** RT #2343 -- Memory foam for my smartphone\n")
+	 (insert-string "* RT #2347 -- Leverage more synergy\n")
+	 (insert-string "** RT #2348 -- Rewrite Emacs in Go\n")
+	 (insert-string "*** RT #2349 -- Port node.js to Java\n")
 	 (goto-char (point-min))
 	 (save-buffer 0)
 	 (funcall body)
@@ -80,17 +80,17 @@
 (ert-deftest yogurty/find-already-existing-rt-ticket-in-org-file-level-1-headline ()
   "Should find already-existing RT ticket in org buffer."
   (my-org-file-fixture
-   (lambda () (should (equal (yogurty-find-rt-ticket-in-org-file "2341") 1)))))
+   (lambda () (should (equal (yogurty-find-rt-ticket-in-org-file "2347") 1)))))
 
 (ert-deftest yogurty/find-already-existing-rt-ticket-in-org-file-level-2-headline ()
   "Should find already-existing RT ticket in org buffer."
   (my-org-file-fixture
-   (lambda () (should (equal (yogurty-find-rt-ticket-in-org-file "2342") 2)))))
+   (lambda () (should (equal (yogurty-find-rt-ticket-in-org-file "2348") 2)))))
 
 (ert-deftest yogurty/find-already-existing-rt-ticket-in-org-file-level-3-headline ()
   "Should find already-existing RT ticket in org buffer."
   (my-org-file-fixture
-   (lambda () (should (equal (yogurty-find-rt-ticket-in-org-file "2343") 3)))))
+   (lambda () (should (equal (yogurty-find-rt-ticket-in-org-file "2349") 3)))))
 
 ;; FIXME: I *think* I've got this working; I think the progn was
 ;; messing things up (different eval order/precedence than I was
