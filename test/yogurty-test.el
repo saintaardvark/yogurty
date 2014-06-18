59dcab7325d8eb4866e46a6e5592be46abc76bf4
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2014-06-18 08:09:38 -0700

    Add FIXME.

--- test/yogurty-test.el
+++ test/yogurty-test.el
@@ -65,6 +65,8 @@
 
 ;; Name for tests: yogurty-test/function-name-short-description-if-necessary
 
+;; FIXME: Need sad-path tests for this -- give it a bogus string and
+;; make sure it fails.
 (ert-deftest yogurty-test/find-ticket-number-from-string ()
   "Should find ticket subject from string."
   (should (equal (yogurty-find-rt-ticket-number-from-string rt-test-string) "2341")))
