123fa7e63dd7a10fa6d5bf8f769199959df9e7ff
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2014-06-27 08:50:51 -0700

    Clean up notes a bit.

--- test/yogurty-test.el
+++ test/yogurty-test.el
@@ -231,7 +231,6 @@
 ;; - clocking in works
 ;; - *not* clocking in works.
 
-;; FIXME: The number 5 is just a guess.
 (ert-deftest yogurty-test/insert-rt-ticket-into-org-from-rt-email-already-existing-ticket ()
   "Make sure ticket ends up in org file."
   (my-org-file-fixture
@@ -250,11 +249,13 @@
 	(yogurty-insert-rt-ticket-into-org-from-rt-email)
 	(should (equal (yogurty-clocked-into-rt-ticket) "RT #2355")))))))
 
-;; This test fails, but only if run in after
-;; yogurty-test/clocked-into-rt-ticket-number-only-new-ticket.  Weird.
-;; If run on its own it works; if run as part of big batch it fails;
-;; if run after clocked-into,  it fails.
-(ert-deftest yogurty-test/clocked-into-rt-ticket-sad-path-not-clocked-in ()
+;; This test failed, but only if run in after
+;; yogurty-test/clocked-into-rt-ticket-number-only-new-ticket.  If run
+;; on its own it worked; if run as part of big batch it failed; if run
+;; after clocked-into, it failed.  I added org-clock-out to the
+;; fixtuure above, a that seems to have fixed things.  However, I'm
+;; leaving this note here because I'm suspicious that it'll crop up
+;; again.
   "Make sure we can detect when we're clocked in."
   (my-org-file-fixture
    (lambda ()
