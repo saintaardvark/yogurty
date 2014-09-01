5d4c43b55bd9ee7ed1839d1a934fad2c1c9d3188
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2014-09-01 16:11:53 -0700

    Working test.

--- test/schedule-rt-ticket-for-today-from-rt-email-test.el
+++ test/schedule-rt-ticket-for-today-from-rt-email-test.el
@@ -12,13 +12,17 @@
 	  (message (buffer-string))
 	  (should (looking-at "foo bar baz"))))))))
 
-(ert-deftest yogurty-test/schedule-rt-ticket-for-today-generic ()
+(ert-deftest yogurty-test/schedule-rt-ticket-for-today-generic-no-previous-schedule ()
   "Make sure RT ticket scheduled for today."
   (my-org-file-fixture
    (lambda ()
      (yogurty-schedule-rt-ticket-for-today-generic 2348)
      (find-file yogurty-org-file)
+     (save-buffer)
      (goto-line (yogurty-find-rt-ticket-in-org-file "2348"))
      (next-line)
-     (message (buffer-string))
-     (should (looking-at "SCHEDULED: <2014-08-31 Sun>")))))
+     (forward-word)
+     (backward-word)
+     ;; FIXME: Should use Org date string stuff
+     (should (looking-at (format-time-string "SCHEDULED: <%Y-%m-%d %a>"))))))
+
