6e500633127f39be5ba28b1266dcd42d301919b1
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2014-09-01 16:27:19 -0700

    Add test for overwriting previous scheduling.

--- test/schedule-rt-ticket-for-today-from-rt-email-test.el
+++ test/schedule-rt-ticket-for-today-from-rt-email-test.el
@@ -29,3 +29,19 @@
      ;; FIXME: Should use Org date string stuff
      (should (looking-at (format-time-string "SCHEDULED: <%Y-%m-%d %a>"))))))
 
+(ert-deftest yogurty-test/schedule-rt-ticket-for-today-generic-overwrite-previous-schedule ()
+  "Make sure we override any previous scheduling."
+  (my-org-file-fixture
+   (lambda ()
+     (goto-line (yogurty-find-rt-ticket-in-org-file "2348"))
+     (org-schedule nil "-7d")
+     (message (buffer-string))
+     (save-buffer)
+     (yogurty-schedule-rt-ticket-for-today-generic 2348)
+     (find-file yogurty-org-file)
+     (save-buffer)
+     (next-line)
+     (forward-word)
+     (backward-word)
+     ;; FIXME: Should use Org date string stuff
+     (should (looking-at (format-time-string "SCHEDULED: <%Y-%m-%d %a>"))))))
